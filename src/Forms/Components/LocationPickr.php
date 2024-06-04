88a47d713bd103bfaf0937b9449c647c23b77d69
zajinx <matei.georgian04@gmail.com>
2024-06-05 00:29:59 +0300

    Update LocationPickr.php
    add groundOverlayUrl and groundOverlayBounds

--- src/Forms/Components/LocationPickr.php
+++ src/Forms/Components/LocationPickr.php
@@ -26,7 +26,11 @@ class LocationPickr extends Field
     protected string | Closure $height = '400px';
 
     protected string | Closure | null $myLocationButtonLabel = null;
+    
+    protected string | Closure $groundOverlayUrl = '';
 
+    protected array | Closure | null $groundOverlayBounds = [0, 0, 0, 0];
+    
     private array $mapConfig = [
         'draggable' => true,
         'clickable' => false,
@@ -39,6 +43,13 @@ class LocationPickr extends Field
         'defaultZoom' => 8,
         'myLocationButtonLabel' => '',
         'apiKey' => '',
+        'groundOverlayUrl' => '',
+        'groundOverlayBounds' => [
+            'north' => 0,
+            'south' => 0,
+            'west' => 0,
+            'east' => 0,
+        ],
     ];
 
     public array $controls = [
@@ -158,6 +169,51 @@ class LocationPickr extends Field
         return $this->evaluate($this->myLocationButtonLabel) ?? config('filament-locationpickr-field.my_location_button');
     }
 
+   public function groundOverlayUrl(string | Closure $groundOverlayUrl): static
+    {
+        $this->groundOverlayUrl = $groundOverlayUrl;
+
+        return $this;
+    }
+
+    public function getGroundOverlayUrl(): string
+    {
+        $groundOverlayUrl = $this->evaluate($this->groundOverlayUrl);
+
+        if (is_string($groundOverlayUrl)) {
+            return $groundOverlayUrl;
+        }
+
+        return config('filament-locationpickr-field.groundOverlayUrl');
+    }
+
+    public function groundOverlayBounds(array | Closure $groundOverlayBounds): static
+    {
+        $this->groundOverlayBounds = $groundOverlayBounds;
+
+        return $this;
+    }
+
+    public function getGroundOverlayBounds(): array
+    {
+        $position = $this->evaluate($this->groundOverlayBounds);
+
+        if (is_array($position)) {
+            if (array_key_exists('north', $position) && array_key_exists('south', $position) && array_key_exists('east', $position) && array_key_exists('west', $position)) {
+                return $position;
+            } elseif (is_numeric($position[0]) && is_numeric($position[1]) && is_numeric($position[2]) && is_numeric($position[3])) {
+                return [
+                    'north' => is_string($position[0]) ? round(floatval($position[0]), $this->precision) : $position[0],
+                    'south' => is_string($position[1]) ? round(floatval($position[1]), $this->precision) : $position[1],
+                    'west' => is_string($position[2]) ? round(floatval($position[2]), $this->precision) : $position[2],
+                    'east' => is_string($position[3]) ? round(floatval($position[3]), $this->precision) : $position[3],
+                ];
+            }
+        }
+
+        return config('filament-locationpickr-field.groundOverlayBounds');
+    }
+
     /**
      * @throws JsonException
      */
@@ -173,6 +229,8 @@ class LocationPickr extends Field
                 'defaultZoom' => $this->getDefaultZoom(),
                 'myLocationButtonLabel' => $this->getMyLocationButtonLabel(),
                 'apiKey' => config('filament-locationpickr-field.key'),
+                'groundOverlayUrl' => $this->getGroundOverlayUrl(),
+                'groundOverlayBounds' => $this->getGroundOverlayBounds(),
             ]),
             JSON_THROW_ON_ERROR
         );
